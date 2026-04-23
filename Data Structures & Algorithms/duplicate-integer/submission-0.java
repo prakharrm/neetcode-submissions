98c5107a6edd78e710def07e55dbfd0782c8f8e7
Prakhar Mahant <143868123+prakharrm@users.noreply.github.com>
2026-04-24 02:24:40 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,16 @@
+
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> map = new HashSet<>();
+
+        for(int i:nums) {
+            if(map.contains(i)) {
+                return true;
+            } else {
+                map.add(i);
+            }
+        }
+
+        return false;
+    }
+}
